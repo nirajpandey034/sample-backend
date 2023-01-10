750d93abf1d8d0b5a3b891bb3291b41e9989027a
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2023-01-10 21:54:38 +0530

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({ info: 'Welcome to Sample Backend' });
