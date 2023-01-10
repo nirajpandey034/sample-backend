9e26d97e90eafb46c03c105bc68472c135380bef
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2023-01-11 00:50:47 +0530

    some swagger changes

--- swagger.js
+++ swagger.js
@@ -1,9 +1,20 @@
 
 const swaggerAutogen = require('swagger-autogen')()
 
+
+const doc = {
+    info: {
+        title: 'Sample Backend',
+        description: 'Sample backend for beginner frontend developers (Web/Mobile)'
+    },
+    host: 'fair-tick-pleat.cyclic.app',
+    basePath: '/',
+    schemes:['https'],
+}
+
 const outputFile = './swagger_output.json'
 const endpointsFiles = ['./app.js']
 
-swaggerAutogen(outputFile, endpointsFiles).then(() => {
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
     require('./app.js')
 })
