ac541006ed396ad43cf3b2eb5ae04468369619c0
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2023-01-10 23:17:17 +0530

    Added swagger

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
+
 const usersRoute = require('./routes/User.js');
 const postsRoute = require('./routes/Post.js');
 const photoRoute = require('./routes/Photo.js');
@@ -15,6 +19,8 @@ app.use('/users', usersRoute);
 app.use('/posts', postsRoute);
 app.use('/photos', photoRoute);
 app.use('/todos', TodoRoute);
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.get('/', (req, res) => {
   res.json({ info: 'Welcome to Sample Backend' });
 });
